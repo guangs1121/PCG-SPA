82ced56e06e25984eaf11706afd41bb412c50b3e
guangs1121 <116379330+guangs1121@users.noreply.github.com>
2024-08-23 11:26:50 -0700

    Update app.js
    change .. to .

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const authRoutes = require('../models/routes/authRoutes');
+const authRoutes = require('./models/routes/authRoutes');
 const ownerRoutes = require('./models/routes/ownerRoutes');
 const landHoldingRoutes = require('./models/routes/landHoldingRoutes');
-const fileRoutes = require('../models/routes/fileRoutes');
+const fileRoutes = require('./models/routes/fileRoutes');
 const app = express();
 
 // Middleware
